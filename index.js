6374777f909b77b83f62df5c592b2d410b188311
Bhavya Shah <bhavya0409@gmail.com>
2026-04-10 01:14:58 -0400

    Added cron

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 import "dotenv/config";
 import { Client, GatewayIntentBits, Events } from "discord.js";
+import cron from "node-cron";
 
 const CLIENT = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
 });
 
+const sendMessage = async (user) => await user.send("Drink water!");
+
 CLIENT.once(Events.ClientReady, async (client) => {
   const user = await client.users.fetch(process.env.USER_ID);
-  await user.send("Drink water!");
-  client.destroy();
+
+  // Send message every hour from 8pm to 1am EST
+  cron.schedule("0 20-23 * * *", () => sendMessage(user), {
+    timezone: "America/New_York",
+  });
+  cron.schedule("0 0-1 * * *", () => sendMessage(user), {
+    timezone: "America/New_York",
+  });
 });
 
 CLIENT.login(process.env.BOT_TOKEN);
